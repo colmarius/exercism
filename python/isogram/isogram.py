9b80eeefe6829771811637872d15ea47f1ff12be
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-05-23 21:54:12 +0200

    Improved solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,12 +1,4 @@
-def is_isogram(string):
-    letters = normalized_letters(string)
-    return same_length(set(letters), letters)
-
-
-def same_length(first, second):
-    return len(first) == len(second)
-
-
-def normalized_letters(string):
-    normalized = string.lower().strip().replace(' ', '').replace('-', '')
-    return list(normalized)
+def is_isogram(phrase):
+    alpha_phrase = filter(str.isalpha, phrase.lower())
+    unique_letters = set(alpha_phrase)
+    return len(unique_letters) == len(alpha_phrase)
