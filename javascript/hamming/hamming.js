7e8e39e116ccc3fe8fd6cf96287fb7d112c672d5
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-06-16 00:54:00 +0200

    Refactor: better naming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,10 +4,10 @@ Hamming.prototype.compute = function(left, right) {
   if (left.length !== right.length)
     throw new Error('DNA strands must be of equal length.')
 
-  const leftLetters = Array.from(left)
-  const rightLetters = Array.from(right)
-  const distances = leftLetters.map((letter, index) => (
-    rightLetters[index] === letter ? 0 : 1
+  const leftStrand = Array.from(left)
+  const rightStrand = Array.from(right)
+  const distances = leftStrand.map((letter, index) => (
+    rightStrand[index] === letter ? 0 : 1
   ))
 
   return distances.reduce((acc, val) => acc + val, 0)
